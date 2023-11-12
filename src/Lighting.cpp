ef3e6864a7462cba48d3fbabbf2447b03fc8da4b
Fady Khairallah <fady@deligent.com>
2023-11-12 16:36:06 -0500

    x

--- src/Lighting.cpp
+++ src/Lighting.cpp
@@ -13,107 +13,54 @@ int ledMode;     // mode of display 1 - 2700K, 2 - rainbow,
 CRGB leds[30];
 long lastLEDUpdate; // hold last time update was sent to LED
 
-// Declare our NeoPixel strip object:
-//Adafruit_NeoPixel *topShelf;
-// Adafruit_NeoPixel bottomShelf(NUM_LEDS_BOTTOM, LED_DATA_PIN_BOTTOM, NEO_GRB + NEO_KHZ800);
-//  Argument 1 = Number of pixels in NeoPixel strip
-//  Argument 2 = Arduino pin number (most are valid)
-//  Argument 3 = Pixel type flags, add together as needed:
-//    NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//    NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//    NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//    NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//    NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-//uint32_t rgbList[] = {topShelf->Color(255, 0, 0), topShelf->Color(0, 255, 0), topShelf->Color(0, 0, 255)};
-
 void configureLED()
 {
 
   console.printf("Configuring top shelf with %s LEDs\r\n", topLED);
-  FastLED.addLeds<WS2811,LED_DATA_PIN_TOP, BRG>(leds, atoi(topLED));
-  //topShelf = new Adafruit_NeoPixel(atoi(topLED), LED_DATA_PIN_TOP, NEO_GBR + NEO_KHZ800);
-  //topShelf->begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  //topShelf->clear(); // Turn OFF all pixels ASAP
+  FastLED.addLeds<WS2811, LED_DATA_PIN_TOP, BRG>(leds, atoi(topLED));
   FastLED.clear();
   FastLED.show();
-  
+
   ledStripON = false;
   ledMode = 1;
   executeLED();
-
 }
 
 // show a green pattern on the LED strip
 void testLED()
 {
-
-  //leds[0] = CRGB::Red; FastLED.show(); delay(1000);
-  console.println("Red");
-  FastLED.showColor(CRGB::Red); 
-  delay(1000);
-  console.println("Green");
-  FastLED.showColor(CRGB::Green);
-  delay(1000);
-  console.println("Blue");
-  FastLED.showColor(CRGB::Blue);
-  delay(1000);
-  console.println("White");
-  FastLED.showColor(CRGB::AntiqueWhite);
+  ledMode = 5;
+  executeLED();
   delay(5000);
-  for (int i=0;i<5;i++) {
+  ledMode = 1;
+
+  // // leds[0] = CRGB::Red; FastLED.show(); delay(1000);
+  // console.println("Red");
+  // FastLED.showColor(CRGB::Red);
+  // delay(1000);
+  // console.println("Green");
+  // FastLED.showColor(CRGB::Green);
+  // delay(1000);
+  // console.println("Blue");
+  // FastLED.showColor(CRGB::Blue);
+  // delay(1000);
+  // console.println("White");
+  // FastLED.showColor(CRGB::AntiqueWhite);
+  // delay(5000);
+  for (int i = 0; i < 5; i++)
+  {
     FastLED.setTemperature(Candle);
-  FastLED.showColor(CRGB::White);
-  delay(1000);
+    FastLED.showColor(CRGB::White);
+    delay(1000);
 
-FastLED.setTemperature(Tungsten100W);
-  FastLED.showColor(CRGB::White);
-  delay(1000);
-FastLED.setTemperature(Tungsten40W);
-  FastLED.showColor(CRGB::White);
-  delay(1000);
-
+    FastLED.setTemperature(Tungsten100W);
+    FastLED.showColor(CRGB::White);
+    delay(1000);
+    FastLED.setTemperature(Tungsten40W);
+    FastLED.showColor(CRGB::White);
+    delay(1000);
   }
-  
 
-  
-
-  // topShelf->clear();
-
-  // console.println("255,0,0");
-  // stripFill(topShelf->gamma32(topShelf->Color(255,0,0)));
-  // topShelf->show();
-  // delay(1000);
-
-  // for (int i = 0; i < topShelf->numPixels(); i++)
-  // {
-
-  //   topShelf->setPixelColor(i, topShelf->Color(255, 0, 0));
-  //   topShelf->show();
-  //   delay(500);
-  // }
-
-  // delay(1000);
-  // topShelf->clear();
-  // console.println("0,255,0!");
-
-  // for (int i = 0; i < topShelf->numPixels(); i++)
-  // {
-
-  //   topShelf->setPixelColor(i, topShelf->Color(0, 255, 0));
-  //   topShelf->show();
-  //   delay(500);
-  // }
-  // delay(1000);
-  // topShelf->clear();
-  // console.println("0,0,255!");
-
-  // for (int i = 0; i < topShelf->numPixels(); i++)
-  // {
-
-  //   topShelf->setPixelColor(i, topShelf->Color(0, 0, 255));
-  //   topShelf->show();
-  //   delay(500);
-  // }
 }
 
 /*
@@ -154,26 +101,22 @@ void executeLED()
   {
     switch (ledMode)
     {
-    case 1:                                    // 100W tungsten -- Pam's favorite
-      //stripFill(topShelf->Color(0, 150, 255)); // bGR
-      // strip.show();
-        FastLED.setTemperature(Candle);
-
+    case 1: // 100W tungsten -- Pam's favorite
+      FastLED.setTemperature(Candle);
       FastLED.showColor(CRGB::White);
       console.println("ON");
       break;
     case 2: // dimmed 50W tungsten
-      //stripFill(topShelf->Color(0, 150 / 2, 255 / 2));
-      // strip.show();
+      FastLED.setTemperature(Candle);
+      FastLED.showColor(CRGB::Grey);
       console.println("DIMMED");
       break;
     case 3: // navy blue
-      //stripFill(topShelf->Color(0, 128, 128));
-      // strip.show();
+      FastLED.showColor(CRGB(0,128,128));
       console.println("3 - dimmmmmm");
       break;
     case 4: // Xmas
-      //fillList(rgbList, 2);
+      // fillList(rgbList, 2);
       console.println("XMAS");
       break;
     case 5: // Rainbow
@@ -182,17 +125,13 @@ void executeLED()
       console.println("RAINBOW");
       break;
     default: // full white
-      //stripFill(topShelf->gamma32(topShelf->Color(255, 255, 255)));
-      // strip.show();
+      FastLED.showColor(CRGB::White);
       console.println("Default");
     }
   }
   else
   {
-    //stripFill(topShelf->gamma32(topShelf->Color(0, 0, 0)));
-    // strip.clear();
-    //topShelf->show();
-    FastLED.clear();
+    fadeToBlackBy(leds, atoi(topLED), 20);
     FastLED.show();
     console.println("OFF");
   }
@@ -232,18 +171,7 @@ void colorWipe(uint32_t color, int wait)
 // fill the strip with a sequence of RGB color
 void fillRainbow()
 {
-
-  // for (int i = 0; i < topShelf->numPixels(); i++)
-  // { // For each pixel in strip...
-  //   if ((i % 3) == 0)
-  //     topShelf->setPixelColor(i, topShelf->Color(255, 0, 0)); //  Red
-  //   if ((i % 3) == 1)
-  //     topShelf->setPixelColor(i, topShelf->Color(0, 255, 0)); //  Green
-  //   if ((i % 3) == 2)
-  //     topShelf->setPixelColor(i, topShelf->Color(0, 0, 255)); //  Blue
-  //   topShelf->show();                                         //  Update strip to match
-  //   delay(50);
-  // }
+  fill_rainbow(leds, atoi(topLED), 0, 5);
 }
 
 // fills top shelf with a sequence of color stored in list[]
